ee7208ec4607ec6f1d521ec9c581d1799a2b385c
Yan <romanets2003@bk.ru>
2023-04-03 11:12:34 +0300

    'init'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import SwiperCore, {Navigation, Pagination} from 'swiper';
 
+SwiperCore.use([Navigation, Pagination])
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'ang';
+  private btn: any;
+  mouseEnter(event: { pageX: number; pageY: number; }){
+    this.btn = document.querySelector('.ripple__btn');
+    const x = event.pageX - this.btn.offsetLeft;
+    const y = event.pageY - this.btn.offsetTop;
+    this.btn.style.setProperty('--x', x + 'px')
+    this.btn.style.setProperty('--y', y + 'px')
+    console.log('enter')
+  }
+  title = 'YANRE';
 }
